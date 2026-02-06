7a6a82d7b89ab216f359b9178c76913d6057f29b
mpark1994 <mpark1994@outlook.com>
2026-02-06 07:32:00 -0500

    Update App.jsx
    import spelling error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom'
 import './App.css'
 
 import HomePage from "./scenes/homePage"
-import ShoppingCart from "./scenes/shoppingCart/index.jsx";
+import ShoppingCart from "./scenes/shoppingCart/Index.jsx";
 import Return from "./scenes/shoppingCart/Return.jsx";
 
 function App() {
